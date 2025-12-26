0d5469358721d5a9353255c910e145ad4fa1b880
LibertyReman <libertyreman@gmail.com>
2025-12-27 04:03:58 +0900

    ButtonコンポーネントにPropsを設定

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import './App.css'
 import Button from './Button'
 
 function App() {
+  // 関数を定義
+  const handleClick = () => {
+    alert('クリックされました');
+  }
+
   return (
     // 空のフラグメント returnの中で複数のタグを書きたい場合に必要
     <>
       <h1>Hello world</h1>
-      <Button />
+      {/* 値の場合は{}が必要 */}
+      <Button type="submit" disabled={false} onClick={handleClick}>
+        <span>ボタンクリック</span>
+      </Button>
     </>
   )
 }
--- src/Button.jsx
+++ src/Button.jsx
@@ -1,15 +1,18 @@
-function Button() {
+// 親コンポーネントから子コンポーネントへ値を渡す仕組み
+// 属性や子要素、イベントなどを親コンポーネントから渡せる
+function Button(props) {
+
+  // children：コンポーネントの中身に書いた要素やテキストを受け取る
+  const {type, disabled, children, onClick} = props;
+
   // 関数を定義
-  const handleClick = () => {
-    alert('クリックされました');
-  }
 
   // returnの中でHTMLを書く
   return (
     // iやspanタグはbuttonの子要素のため空のフラグメントは不要
-    <button type="button" onClick={handleClick}>
+    <button type={type} disabled={disabled} onClick={onClick}>
       <i>icon</i>
-      <span>クリック</span>
+      {children}
     </button>
   )
 }
