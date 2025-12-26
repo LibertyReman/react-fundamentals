e2c7e32bb4f060f4ba50679229c50cc418b54d92
LibertyReman <libertyreman@gmail.com>
2025-12-27 04:55:04 +0900

    useStateを使ったButtonカウント用の状態管理を親コンポーネントで実施

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
+import {useState} from "react"
 import './App.css'
 import Button from './components/Button/Button'
 
 function App() {
+  // 状態を持たせたい変数とそれを更新する関数を定義
+  // 状態は親から子コンポーネントに渡すのが基本設計親
+  const [count, setCount] = useState(0);
+
   // 関数を定義
+  const handleClick = () => {
+    setCount(count + 1);
+  }
 
   return (
     // 空のフラグメント returnの中で複数のタグを書きたい場合に必要
     <>
       <h1>Hello world</h1>
       {/* 値の場合は{}が必要 */}
-      <Button type="submit" disabled={false}/>
+      <Button type="submit" disabled={false} onClick={handleClick}>
+        カウント：{count}
+      </Button>
     </>
   )
 }
