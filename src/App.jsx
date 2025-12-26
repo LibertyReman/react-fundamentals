8a5aa4f6067ebac04fb488388df2ecfc3337ec60
LibertyReman <libertyreman@gmail.com>
2025-12-27 04:40:14 +0900

    useStateを使ってButtonカウント用の状態管理を追加

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,13 @@ import Button from './components/Button/Button'
 
 function App() {
   // 関数を定義
-  const handleClick = () => {
-    alert('クリックされました');
-  }
 
   return (
     // 空のフラグメント returnの中で複数のタグを書きたい場合に必要
     <>
       <h1>Hello world</h1>
       {/* 値の場合は{}が必要 */}
-      <Button type="submit" disabled={false} onClick={handleClick}>
-        <span>ボタンクリック</span>
-      </Button>
+      <Button type="submit" disabled={false}/>
     </>
   )
 }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,20 +1,26 @@
 import styles from "./Button.module.css";
+import {useState} from "react"
 
 // 親コンポーネントから子コンポーネントへ値を渡す仕組み
 // 属性や子要素、イベントなどを親コンポーネントから渡せる
 function Button(props) {
 
+  // 状態を持たせたい変数とそれを更新する関数を定義
+  const [count, setCount] = useState(0);
+
   // children：コンポーネントの中身に書いた要素やテキストを受け取る
-  const {type, disabled, children, onClick} = props;
+  const {type, disabled} = props;
 
   // 関数を定義
+  const handleClick = () => {
+    setCount(count + 1);
+  }
 
   // returnの中でHTMLを書く
   return (
     // iやspanタグはbuttonの子要素のため空のフラグメントは不要
-    <button className={styles.buttonInner} type={type} disabled={disabled} onClick={onClick}>
-      <i>icon</i>
-      {children}
+    <button className={styles.buttonInner} type={type} disabled={disabled} onClick={handleClick}>
+      カウント：{count}
     </button>
   )
 }
