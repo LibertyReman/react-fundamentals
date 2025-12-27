335b27a58db6ce48ab95311bb2755e68d46362d6
LibertyReman <libertyreman@gmail.com>
2025-12-27 09:12:14 +0900

    React Routerを導入し、ページ遷移を追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,17 @@
-import {useState, useEffect} from "react"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import './App.css'
-import Button from './components/Button/Button'
-import Display from './components/Display/Display'
+import Home from './pages/Home'
+import SamplePage from './pages/SamplePage'
 
 function App() {
-  // 状態を持たせたい変数とそれを更新する関数を定義
-  // 状態は親から子コンポーネントに渡すのが基本設計親
-  const [count, setCount] = useState(0);
-
-  // 関数を定義
-  const handleClick = () => {
-    setCount(count + 1);
-  }
-
-  // useEffectは関数内かつreturnの前に定義
-  // 第1引数にコールバック関数、第2引数に配列を指定（空の場合はコンポーネントが初回表示されたときだけ実行）
-  // countが変更されたときだけ実行
-  //useEffect(()=>{
-  //  console.log(count);
-  //  if(count > 15) {
-  //    setCount(0);
-  //  }
-  //}, [count])
-
   return (
     // 空のフラグメント returnの中で複数のタグを書きたい場合に必要
-    <>
-      <h1>Hello world</h1>
-      {/* 値の場合は{}が必要 */}
-      <Button type="submit" disabled={false} onClick={handleClick}>
-        ボタン
-      </Button>
-      <Display count={count} />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/sample-page' element={<SamplePage />}/>
+      </Routes>
+    </BrowserRouter >
   )
 }
 
---
+++ src/pages/SamplePage.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function SamplePage(){
+  return (
+    <>
+      <h1>SamplePage</h1>
+      <Link to="/" target="_blank">Go to Home</Link>
+    </>
+  )
+}
+export default SamplePage;
